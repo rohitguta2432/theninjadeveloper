c303ec53066c8615ecd1b3b82c36143b5448f814
rohit raj <rohitgupta2432@gmail.com>
2020-08-20 00:00:43 +0530

    initial commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Header/Header.css'
+
+function Header() {
+    return (
+        <nav className="header">
+            The Ninja Developer
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Home/Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__left">
+             <img className="home__ninja_img"
+                    src="https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg" alt="" /> 
+            </div>
+            <div className="home__right">
+                Jobs
+                    </div>
+        </div>
+    )
+}
+
+export default Home
