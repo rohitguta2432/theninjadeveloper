892a553615843c619e739af93883cd0cc77e60df
rohit <rohitgupta2432@gmail.com>
2020-08-23 15:03:36 +0530

    resolve conflicts

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import '../Home/Home.css'
+import { Button } from 'react-bootstrap'
 function Home() {
     return (
         <div className="home">
-            <div className="home__left">
-             <img className="home__ninja_img"
-                    src="https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg" alt="" /> 
+            <div className="home__child">
+                <Button>fwefweffe</Button>
             </div>
-            <div className="home__right">
-                Jobs
-                    </div>
         </div>
     )
 }
