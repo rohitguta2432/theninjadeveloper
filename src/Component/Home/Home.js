b286171994a4da1c63b650c297311b46cdb0814d
rohit <rohitgupta2432@gmail.com>
2020-08-23 15:16:54 +0530

    added card on the dashoard

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import '../Home/Home.css'
 import { Button } from 'react-bootstrap'
+import {Card} from 'react-bootstrap'
 function Home() {
     return (
         <div className="home">
             <div className="home__child">
-                <Button>fwefweffe</Button>
+                <Card>
+                    <Card.Img/>
+                    <Card.Body>
+                        <Card.Title>Java Developer</Card.Title>
+                        <Card.Text>
+                            Having 5 + years Of Experience Java developer
+                         </Card.Text>
+                        <Button variant="primary">View Profile</Button>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     )
 }
-
 export default Home
